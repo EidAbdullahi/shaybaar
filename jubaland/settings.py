4e3297aaf15f35aff501e56c0ba3a53744603b8b
EidAbdullahi <eidabdullahi10@gmail.com>
2023-01-19 11:23:32 +0200

    commit changes

--- jubaland/settings.py
+++ jubaland/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -141,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
